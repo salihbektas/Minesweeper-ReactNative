77f822a439a990759748eb669ec368b926dd955c
Salih Bektaş <sbektas@gtu.edu.tr>
2023-06-24 16:41:22 +0300

    style: dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -5,16 +5,21 @@ export default function Dashboard({difficulty, numOfFlags, numOfActiveMines}) {
 
     return (
         <View style={{ flexDirection: "row", justifyContent: "space-evenly", width: "80%", backgroundColor: "lightgrey", paddingVertical: 8, borderRadius: 6 }}>
-            <Text style={{ fontSize: 24 }} >{options[difficulty].numberOfMine}</Text>
-            <Image source={require("../../assets/mine.png")} style={{ width: 30, aspectRatio: 1, resizeMode: "contain" }} />
-            <Text style={{ fontSize: 24 }} >-</Text>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={{ fontSize: 28 }} >{options[difficulty].numberOfMine}</Text>
+                <Image source={require("../../assets/mine.png")} style={{ width: 30, aspectRatio: 1, resizeMode: "contain", marginLeft: 5, marginTop: 5 }} />
+            </View>
 
-            <Text style={{ fontSize: 24 }} >{numOfFlags}</Text>
-            <Image source={require("../../assets/redFlag.png")} style={{ width: 30, aspectRatio: 1, resizeMode: "contain" }} />
+            <Text style={{ fontSize: 28 }} >-</Text>
 
-            <Text style={{ fontSize: 24 }} >=</Text>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={{ fontSize: 28 }} >{numOfFlags}</Text>
+                <Image source={require("../../assets/redFlag.png")} style={{ width: 30, aspectRatio: 1, resizeMode: "contain", marginLeft: 5, marginTop: 5 }} />
+            </View>
 
-            <Text style={{ fontSize: 24 }} >{numOfActiveMines}</Text>
+            <Text style={{ fontSize: 28 }} >=</Text>
+
+            <Text style={{ fontSize: 28 }} >{numOfActiveMines}</Text>
 
         </View>
     )
