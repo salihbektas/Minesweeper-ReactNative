d8b7c76a47903feb4afb96a7fee0f08477e5f9bf
Salih Bektaş <sbektas@gtu.edu.tr>
2023-07-02 23:00:38 +0300

    feat: settings and records separated

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -8,8 +8,11 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { formatTime } from "../../utils";
 
 
+const tabs = ['Settings', 'Records']
 
 export default function Settings({ navigation }) {
+
+  const [selectedTab, setSelectedTab] = useState(0)
 
   const [data, setData] = useAtom(store)
 
@@ -32,14 +35,14 @@ export default function Settings({ navigation }) {
 
   function changeTheme(isDarkMode) {
 
-    AsyncStorage.setItem('DarkMode', JSON.stringify(isDarkMode())).catch(err => {console.error("error on save DarkMode", err)})
-    setData(d => ({...d, darkMode: isDarkMode()}))
+    AsyncStorage.setItem('DarkMode', JSON.stringify(isDarkMode())).catch(err => { console.error("error on save DarkMode", err) })
+    setData(d => ({ ...d, darkMode: isDarkMode() }))
   }
 
   function changeDifficulty(selectedDifficulty) {
     let newDifficulty = selectedDifficulty()
-    AsyncStorage.setItem('Difficulty', JSON.stringify(newDifficulty)).catch(err => {console.error("error on save Difficulty", err)})
-    setData((data) => ({...data, difficulty: newDifficulty}))
+    AsyncStorage.setItem('Difficulty', JSON.stringify(newDifficulty)).catch(err => { console.error("error on save Difficulty", err) })
+    setData((data) => ({ ...data, difficulty: newDifficulty }))
   }
 
   return (
@@ -48,47 +51,76 @@ export default function Settings({ navigation }) {
         <Pressable onPress={() => navigation.goBack()} >
           <Image source={require("../../../assets/arrow.png")} style={styles.back(darkMode)} />
         </Pressable>
-        <Text style={styles.settingText(darkMode)}>Settings</Text>
+
+        {tabs.map((item, index) => {
+          const isActive = index === selectedTab;
+
+          return (
+            <View style={{ flex: 1 }} key={index}>
+              <Pressable
+                onPress={() => {
+                  setSelectedTab(index);
+                }}>
+                <View
+                  style={[
+                    styles.item(darkMode),
+                    isActive && { borderBottomColor: colors.tileOpened },
+                  ]}>
+                  <Text style={[styles.settingText(darkMode), isActive && { color: colors.tileOpened }]}>
+                    {item}
+                  </Text>
+                </View>
+              </Pressable>
+            </View>
+          )
+        })}
       </View>
 
       <View style={styles.main} >
 
-        <View style={styles.row}>
-          <Text style={styles.text(darkMode)}>{'Theme:'}</Text>
-          <DropDownPicker
-            open={openTheme}
-            value={darkMode}
-            items={themes}
-            setOpen={setOpenTheme}
-            setValue={changeTheme}
-            onOpen={onThemeOpen}
-            theme={darkMode ? 'LIGHT' : 'DARK'}
-            containerStyle={styles.dropDownPicker}
-            style={styles.dropDownPickerStyle(darkMode)}
-            dropDownContainerStyle={styles.dropDownPickerStyle(darkMode)}
-          />
-        </View>
+        {selectedTab === 0
+          ?
+          <>
+            <View style={styles.row}>
+              <Text style={styles.text(darkMode)}>{'Theme:'}</Text>
+              <DropDownPicker
+                open={openTheme}
+                value={darkMode}
+                items={themes}
+                setOpen={setOpenTheme}
+                setValue={changeTheme}
+                onOpen={onThemeOpen}
+                theme={darkMode ? 'LIGHT' : 'DARK'}
+                containerStyle={styles.dropDownPicker}
+                style={styles.dropDownPickerStyle(darkMode)}
+                dropDownContainerStyle={styles.dropDownPickerStyle(darkMode)}
+              />
+            </View>
 
-        <View style={styles.row}>
-          <Text style={styles.text(darkMode)}>{'Difficulty:'}</Text>
-          <DropDownPicker
-            open={openDifficulties}
-            value={data.difficulty}
-            items={difficulties}
-            setOpen={setOpenDifficulties}
-            setValue={changeDifficulty}
-            onOpen={onColorCodeOpen}
-            theme={darkMode ? 'LIGHT' : 'DARK'}
-            containerStyle={styles.dropDownPicker}
-            style={styles.dropDownPickerStyle(darkMode)}
-            dropDownContainerStyle={styles.dropDownPickerStyle(darkMode)}
-          />
-        </View>
+            <View style={styles.row}>
+              <Text style={styles.text(darkMode)}>{'Difficulty:'}</Text>
+              <DropDownPicker
+                open={openDifficulties}
+                value={data.difficulty}
+                items={difficulties}
+                setOpen={setOpenDifficulties}
+                setValue={changeDifficulty}
+                onOpen={onColorCodeOpen}
+                theme={darkMode ? 'LIGHT' : 'DARK'}
+                containerStyle={styles.dropDownPicker}
+                style={styles.dropDownPickerStyle(darkMode)}
+                dropDownContainerStyle={styles.dropDownPickerStyle(darkMode)}
+              />
+            </View>
+          </>
+          :
+          <>
+            <Text style={styles.feedbackText(darkMode)}>{`Easy: ${data.records.e ? formatTime(data.records.e) : 'not set'}`}</Text>
+            <Text style={styles.feedbackText(darkMode)}>{`Medium: ${data.records.m ? formatTime(data.records.m) : 'not set'}`}</Text>
+            <Text style={styles.feedbackText(darkMode)}>{`hard: ${data.records.h ? formatTime(data.records.h) : 'not set'}`}</Text>
+          </>
+        }
 
-        <Text style={styles.feedbackText(darkMode)}>Records:</Text>
-        <Text style={styles.feedbackText(darkMode)}>{`Easy: ${data.records.e ? formatTime(data.records.e) : 'not set'}`}</Text>
-        <Text style={styles.feedbackText(darkMode)}>{`Medium: ${data.records.m ? formatTime(data.records.m) : 'not set'}`}</Text>
-        <Text style={styles.feedbackText(darkMode)}>{`hard: ${data.records.h ? formatTime(data.records.h) : 'not set'}`}</Text>
 
       </View>
     </View>
@@ -98,7 +130,7 @@ export default function Settings({ navigation }) {
 const styles = StyleSheet.create({
   container: (isDarkMode) => ({
     flex: 1,
-    backgroundColor: isDarkMode ? colors.dark : colors.light,
+    backgroundColor: isDarkMode ? colors.dark : colors.white,
     alignItems: 'center',
     paddingHorizontal: 16
   }),
@@ -121,6 +153,14 @@ const styles = StyleSheet.create({
     color: darkMode ? colors.white : colors.dark,
     fontSize: 32,
     fontWeight: "700"
+  }),
+
+  item: (darkMode) => ({
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderColor: darkMode ? colors.dark : colors.white,
+    borderBottomWidth: 2,
   }),
 
   main: {
